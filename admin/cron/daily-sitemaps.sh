2691777b3867e5021fbb96bda3621eec2170cf0d
Michael Wiencek <mwtuea@gmail.com>
2016-09-13 14:38:42 -0500

    Docker

--- admin/cron/daily-sitemaps.sh
+++ admin/cron/daily-sitemaps.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -u
 
+export PERL_CARTON_PATH=~/carton-local
+
 mb_server=`dirname $0`/../..
 cd $mb_server
 
-OUTPUT=`./admin/BuildSitemaps.pl --ping` || echo "$OUTPUT"
+OUTPUT=`carton exec -- ./admin/BuildSitemaps.pl --ping` || echo "$OUTPUT"
 
 # eof
--- admin/cron/hourly-sitemaps.sh
+++ admin/cron/hourly-sitemaps.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -u
 
+export PERL_CARTON_PATH=~/carton-local
+
 mb_server=`dirname $0`/../..
 cd $mb_server
 
-OUTPUT=`./admin/cron/slave.sh 2>&1` || echo "$OUTPUT"
+OUTPUT=`carton exec -- ./admin/cron/slave.sh 2>&1` || echo "$OUTPUT"
 
 OUTPUT=`
-    ./admin/BuildIncrementalSitemaps.pl --ping --worker-count 7 2>&1
+    carton exec -- ./admin/BuildIncrementalSitemaps.pl --ping --worker-count 7 2>&1
 ` || echo "$OUTPUT"
 
 # eof
